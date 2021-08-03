b148967f3e02e492027ee5947037e71ee07ea354
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-08-03 10:42:06 -0300

    first commit

---
+++ src/main/java/com/raimiyashiro/cloudHumans/CloudHumansApplication.java
@@ -0,0 +1,13 @@
+package com.raimiyashiro.cloudHumans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudHumansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudHumansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raimiyashiro/cloudHumans/CloudHumansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raimiyashiro.cloudHumans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudHumansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

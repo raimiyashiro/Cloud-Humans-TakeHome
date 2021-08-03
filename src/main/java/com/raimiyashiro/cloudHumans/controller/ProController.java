192783e808fe14b505bbdcb4b5a53a758f8d5a5d
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-08-03 19:42:10 -0300

    Injects @EligibilityAlgorithm into ProController

--- src/main/java/com/raimiyashiro/cloudHumans/controller/ProController.java
+++ src/main/java/com/raimiyashiro/cloudHumans/controller/ProController.java
@@ -1,18 +1,41 @@
 package com.raimiyashiro.cloudHumans.controller;
 
+import com.raimiyashiro.cloudHumans.controller.dto.EligibilityScoreDTO;
 import com.raimiyashiro.cloudHumans.controller.dto.ProDTO;
+import com.raimiyashiro.cloudHumans.core.EligibilityAlgorithm;
+import com.raimiyashiro.cloudHumans.data.ProjectDataSource;
 import com.raimiyashiro.cloudHumans.model.EligibilityScore;
+import com.raimiyashiro.cloudHumans.model.Pro;
+import com.raimiyashiro.cloudHumans.model.Project;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/pro")
 public class ProController {
 
+    private final EligibilityAlgorithm algorithm;
+    private final ProjectDataSource projectDataSource;
+
+    @Autowired
+    public ProController(EligibilityAlgorithm algorithm, ProjectDataSource projectDataSource) {
+        this.algorithm = algorithm;
+        this.projectDataSource = projectDataSource;
+    }
+
     @PostMapping("/score")
     @ResponseBody
-    public EligibilityScore getScore(@RequestBody @Valid ProDTO proDTO) {
-        return null;
+    public EligibilityScoreDTO getScore(@RequestBody @Valid ProDTO proDTO) {
+        var mapper = new ModelMapper();
+        var pro = mapper.map(proDTO, Pro.class);
+
+        List<Project> projects = this.projectDataSource.listAvailableProjects();
+        EligibilityScore score = this.algorithm.evaluatePro(pro, projects);
+
+        return mapper.map(score, EligibilityScoreDTO.class);
     }
 }
--- src/main/java/com/raimiyashiro/cloudHumans/core/EligibilityAlgorithm.java
+++ src/main/java/com/raimiyashiro/cloudHumans/core/EligibilityAlgorithm.java
@@ -1,7 +1,16 @@
 package com.raimiyashiro.cloudHumans.core;
 
+import com.raimiyashiro.cloudHumans.model.EligibilityScore;
+import com.raimiyashiro.cloudHumans.model.Pro;
+import com.raimiyashiro.cloudHumans.model.Project;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class EligibilityAlgorithm {
+
+    public EligibilityScore evaluatePro(Pro pro, List<Project> availableProjects) {
+        return null;
+    }
 }

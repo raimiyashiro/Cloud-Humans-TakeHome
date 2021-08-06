77719564678d59ef08257aa97df09362c372b83f
Rai <rai.goncalves.miyashiro@everis.com>
2021-08-06 00:00:24 -0300

    WIP: Testing Algorithm

--- src/test/java/com/raimiyashiro/cloudHumans/core/EligibilityAlgorithmTest.java
+++ src/test/java/com/raimiyashiro/cloudHumans/core/EligibilityAlgorithmTest.java
@@ -1,4 +1,113 @@
+package com.raimiyashiro.cloudHumans.core;
+
+import com.raimiyashiro.cloudHumans.enums.EducationLevelEnum;
+import com.raimiyashiro.cloudHumans.enums.PastExperiencesEnum;
+import com.raimiyashiro.cloudHumans.model.Pro;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 class EligibilityAlgorithmTest {
-  
+
+    EligibilityAlgorithm algorithm;
+
+    @BeforeEach
+    void setup() {
+        algorithm = Mockito.spy(new EligibilityAlgorithm());
+    }
+
+    @Test
+    void evaluateEducationLevel_whenProHasHighSchoolEducationLevel() {
+        Pro pro = new Pro();
+        pro.setEducationLevel(EducationLevelEnum.high_school.name());
+
+        Integer expected = 1;
+        Integer actual = this.algorithm.evaluateEducationLevel(pro);
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void evaluateEducationLevel_whenProHasBachelorsDegreeOrHighEducationLevel() {
+        Pro pro = new Pro();
+        pro.setEducationLevel(EducationLevelEnum.bachelors_degree_or_high.name());
+
+        Integer expected = 2;
+        Integer actual = this.algorithm.evaluateEducationLevel(pro);
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void evaluateEducationLevel_whenProHasNoEducationLevel() {
+        Pro pro = new Pro();
+        pro.setEducationLevel(EducationLevelEnum.no_education.name());
+
+        Integer expected = 0;
+        Integer actual = this.algorithm.evaluateEducationLevel(pro);
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void evaluatePastExperiences_whenProHasExperienceWithSalesAndSupport() {
+        Pro pro = new Pro();
+        Map<String, Boolean> experiences = new HashMap<>();
+
+        experiences.put(PastExperiencesEnum.sales.name(), Boolean.TRUE);
+        experiences.put(PastExperiencesEnum.support.name(), Boolean.TRUE);
+        pro.setPastExperiences(experiences);
+
+        Integer expected = 5;
+        Integer actual = this.algorithm.evaluatePastExperiences(pro);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void evaluatePastExperiences_whenProHasExperienceWithSalesOrSupport() {
+        Pro pro = new Pro();
+        Integer expectedEvaluation = 3;
+
+        Map<String, Boolean> experiences = new HashMap<>();
+
+        experiences.put(PastExperiencesEnum.sales.name(), Boolean.TRUE);
+        pro.setPastExperiences(experiences);
+
+        Integer actual = this.algorithm.evaluatePastExperiences(pro);
+
+        Assertions.assertEquals(expectedEvaluation, actual);
+
+        // Just making sure that it works also with PastExperiencesEnum.Support
+        experiences.remove(PastExperiencesEnum.sales.name());
+        experiences.put(PastExperiencesEnum.support.name(), Boolean.TRUE);
+        pro.setPastExperiences(experiences);
+
+        Assertions.assertEquals(expectedEvaluation, this.algorithm.evaluatePastExperiences(pro));
+    }
+
+    @Test
+    void evaluateInternetSpeed() {
+    }
+
+    @Test
+    void evaluateWritingScore() {
+    }
+
+    @Test
+    void evaluateReferralCode() {
+    }
+
+    @Test
+    void calculateScore() {
+    }
 }

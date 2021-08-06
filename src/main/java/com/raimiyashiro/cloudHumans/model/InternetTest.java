d0fbf0c076e8b46779243c12f2f0d533af2643ca
Rai <rai.goncalves.miyashiro@everis.com>
2021-08-06 10:15:02 -0300

    Bugfix

--- src/main/java/com/raimiyashiro/cloudHumans/model/InternetTest.java
+++ src/main/java/com/raimiyashiro/cloudHumans/model/InternetTest.java
@@ -1,9 +1,12 @@
 package com.raimiyashiro.cloudHumans.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class InternetTest {
+    @JsonProperty("download_speed")
     private float downloadSpeed;
+    @JsonProperty("upload_speed")
     private float uploadSpeed;
 }
